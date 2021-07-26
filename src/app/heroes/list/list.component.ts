ea8d09a3825a0aea728bc34c51882014752d3540
Felipe Gomez <felipegh@outlook.com>
2021-07-25 23:28:42 -0500

    section 4 video 47

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+})
+export class ListComponent
+{
+  heroes: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+  ];
+  heroDeleted: string = "";
+
+
+  deleteHero()
+  {
+    this.heroDeleted = this.heroes.shift() || "";
+  }
+}
